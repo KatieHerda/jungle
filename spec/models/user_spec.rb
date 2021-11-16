bdc3251ebbdee0d773b966dedbff94c9b1bb19ed
Katie <katie.herda2@gmail.com>
2021-11-16 05:06:06 +0000

    validates a user is saved.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,2 +1,15 @@
 require 'rails_helper'
 
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+
+    # initial example that ensures that a user creates with all four fields save
+    it "is a valid user" do
+      @user = User.new(first_name: "Jacky", last_name: "Johnson", email: "jackyj@mail.com", password: "12345678", password_confirmation: "12345678")
+      @user.save
+      expect(@user).to be_valid
+    end
+
+  end
+end
+
