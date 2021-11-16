02b076036193002212f2e611db509010532803f3
Katie <katie.herda2@gmail.com>
2021-11-16 05:50:15 +0000

    updated validations for user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :email,                 presence: true, uniqueness: { case_sensitive: false }
+  validates :password,              presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+  validates :first_name,            presence: true
+  validates :last_name,             presence: true
+ 
 end
