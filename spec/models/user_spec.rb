d69dd76f023834443e7f03e8673c1c3a5fa23bf8
Katie <katie.herda2@gmail.com>
2021-11-16 05:53:26 +0000

    validates password and password confirmation. also validates if they don't match.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -24,14 +24,6 @@ RSpec.describe User, type: :model do
       expect(@user).to_not be_valid
     end
 
-
-    #last name required
-    it "is invalid without a first name" do
-      @user = User.new(first_name: "Jacky", last_name: nil, email: "jackyj@mail.com", password: "12345678", password_confirmation: "12345678")
-      @user.save
-      expect(@user).to_not be_valid
-    end
-
     #email required
     it "is invalid without an email" do
       @user = User.new(first_name: "Jacky", last_name: "Johnson", email: nil, password: "12345678", password_confirmation: "12345678")
@@ -39,6 +31,26 @@ RSpec.describe User, type: :model do
       expect(@user).to_not be_valid
     end
 
+    #invalid without a password
+    it "is invalid without a password" do
+      @user = User.new(first_name: "Jacky", last_name: "Johnson", email: "jackyj@mail.com", password: nil, password_confirmation: "12345678")
+      @user.save
+      expect(@user).to_not be_valid
+    end
+
+    #invalid without a password confirmation
+    it "is invalid without a password" do
+      @user = User.new(first_name: "Jacky", last_name: "Johnson", email: "jackyj@mail.com", password: "12345678", password_confirmation: nil)
+      @user.save
+      expect(@user).to_not be_valid
+    end
+
+    #invalid if password and password confirmation do not match
+      it "is invalid without a password" do
+        @user = User.new(first_name: "Jacky", last_name: "Johnson", email: "jackyj@mail.com", password: "12345678", password_confirmation: "87654321")
+        @user.save
+        expect(@user).to_not be_valid
+      end
   end
 end
 
