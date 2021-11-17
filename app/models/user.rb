29b3e000accf3636185d2f79c2e1538fad9a7376
Katie <katie.herda2@gmail.com>
2021-11-17 05:20:48 +0000

    changed email to downcase before save and before authentication.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,17 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
   validates :first_name,            presence: true
   validates :last_name,             presence: true
- 
+  before_save { email.downcase! }
+
   def self.authenticate_with_credentials(email, password)
     #if successfully authenticated, return instance of the user
 
+    email.downcase!
+    email.strip!
+    puts email
+
     #cleans email for white space and case
-    @user = User.find_by_email(email.downcase.strip)
+    @user = User.find_by_email(email)
     if @user
       #returns password (truthy) if password exists/matches
       if @user.authenticate(password)
