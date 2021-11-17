42400eaefb30979633315cf0e2822358c1603800
Katie <katie.herda2@gmail.com>
2021-11-17 04:13:47 +0000

    added a new feature spec to test that users can add a product to their cart by verifying css in nav bar that updates cart from 0 to 1.

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.feature "Can add a product to the cart", type: :feature, js: true do
+  
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+
+  scenario "Can click the 'add to cart' button and the cart count increases by 1" do
+    # ACT
+    visit root_path
+
+    #VERIFY cart before click
+    expect(page).to have_css('.navbar-default .navbar-nav > li > a', text: "My Cart (0)")
+
+    page.all(".product .actions form")[0].click
+
+    # commented out b/c it's for debugging only
+    save_screenshot
+
+    # VERIFY cart after click
+    expect(page).to have_css('.navbar-default .navbar-nav > li > a', text: "My Cart (1)")
+
+  end
+
+end
